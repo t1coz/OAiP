f005796f2e6f3114958e16eab6c9255afd0debcd
t1coz <66951820+t1coz@users.noreply.github.com>
2023-02-26 12:07:57 +0300

    Delete 2ndTerm/1Laba directory

--- 2ndTerm/1Laba/main.c
+++
@@ -1,8 +0,0 @@
-#include "boilers.h"
-#include "menuCommands.h"
-int main(){
-    int number = 0;
-    Boiler* boilers = NULL;
-    //creatingTheArray(&boilers, number);
-    menuInterface(&boilers, &number);
-}
