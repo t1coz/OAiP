73f30fb65282eb088226d15dd63d8d8cbf253d6e
t1coz <66951820+t1coz@users.noreply.github.com>
2023-04-27 09:57:42 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -482,6 +482,13 @@ void print(Ring* p){
 //    while(temp->left != NULL){ temp = temp->left;}
 //    return temp->key;
 //}
+//struct node* search(struct node* root, int key){
+//    if (root == NULL || root->key == key)
+//        return root;
+//    if (root->key < key)
+//        return search(root->right, key);
+//    return search(root->left, key);
+//}
 int main(){
 
     return 0;
