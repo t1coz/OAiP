64ebe7ec6b8452f8948e1e3878a309c2140f1125
t1coz <66951820+t1coz@users.noreply.github.com>
2023-04-27 09:15:47 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -449,6 +449,39 @@ void print(Ring* p){
 //        }
 //    }
 //}
+//struct node *removeNode(struct node *root, int val){
+//    if(root == NULL)
+//        return NULL;
+//    if(root->key < val)
+//        root->right = removeNode(root->right,val);
+//    else if(root->key > val)
+//        root->left = removeNode(root->left,val);
+//    else{
+//        if(root->left == NULL && root->right == NULL){
+//            free(root);
+//            return NULL;
+//        }else if(root->left == NULL){
+//            struct node *temp = root->right;
+//            free(root);
+//            return temp;
+//        }else if(root->right == NULL){
+//            struct node *temp = root->left;
+//            free(root);
+//            return temp;
+//        }else{
+//            int rightMin = getRightMin(root->right);
+//            root->key = rightMin;
+//            root->right = removeNode(root->right,rightMin);
+//        }
+//
+//    }
+//    return root;
+//}
+//int getRightMin(struct node *root){
+//    struct node *temp = root;
+//    while(temp->left != NULL){ temp = temp->left;}
+//    return temp->key;
+//}
 int main(){
 
     return 0;
